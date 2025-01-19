1222179ad8d669c8e21266cace5986db1ee66d47
taishi29 <115542864+taishi29@users.noreply.github.com>
2025-01-19 15:53:11 +0900

    TopSectionクラス及びMenuListクラスを作成し、大枠を完成させました。ですが、余白などのデザインやボタンなどはハリボテなのでまだまだ改善する余地あり。

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 // DateTimeクラスパッケージ
 import 'package:intl/intl.dart';
 
+import 'dart:async';
+
+// main関数
 void main() {
   runApp(const MyApp());
 }
 
+// アプリの基本設定
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,64 +21,99 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'GymLog'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// 一番最初に表示される画面
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('筋トレ記録')),
+      body: Column(
+        children: [
+          TopSection(title: 'Hello World!'), // 時刻とトレーニング開始時刻
+          Expanded(
+            child: MenuList(),
+          ), // 筋トレメニューリスト
+        ],
+      ),
+    );
+  }
+}
+
+// 現在時刻とトレーニング開始時刻を表示するWidget
+class TopSection extends StatefulWidget {
+  const TopSection({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TopSection> createState() => _TopSectionState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TopSectionState extends State<TopSection> {
+  String nowTime = DateFormat('HH:mm:ss').format(DateTime.now()).toString();
+  int index = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  // 時刻を一秒ごとに更新するための関数
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), _onTimer);
+  }
+
+  void _onTimer(Timer timer) {
+    var newTime = DateFormat('HH:mm:ss').format(DateTime.now());
+    setState(() => nowTime = newTime);
   }
 
   @override
   Widget build(BuildContext context) {
-    // 現在の日付をフォーマット
-    String formattedDate = DateFormat('yyyy年MM月dd日').format(DateTime.now());
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('今日の日付: $formattedDate'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("現在の時刻"),
             Text(
-              '$_counter',
+              nowTime,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {/* ボタンがタップされた時の処理 */},
+              child: Text('トレーニング開始'),
+            ),
+            Text('not depelopment')
+          ],
+        )
+      ],
     );
   }
 }
 
-# マシンリストボタン
-class MachineType extends StatelessWidget {
-  const MachineType({super.key});
-
+// メニューリスト
+class MenuList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+        Text('チェストプレス'),
+      ],
+    );
+  }
 }
