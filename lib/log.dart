1c6df4800265c14cb249da359c4897748fd428f2
taishi29 <115542864+taishi29@users.noreply.github.com>
2025-01-22 17:39:21 +0900

    ListTileをタップした後に、画面遷移をする挙動を作りました。

--- lib/log.dart
+++ lib/log.dart
@@ -1,41 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:gymlog/log.dart';
 // DateTimeクラスパッケージ
 import 'package:intl/intl.dart';
 import 'dart:async';
-// ListViewのアニメーション
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
-// main関数
-void main() {
-  runApp(const MyApp());
-}
-
-// アプリの基本設定
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GymLog',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-// 一番最初に表示される画面
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+class LogPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height; // 画面の高さを取得
-
     return Scaffold(
       appBar: AppBar(
         title: Text('筋トレ記録アプリ'),
@@ -49,7 +20,7 @@ class HomeScreen extends StatelessWidget {
             child: TopSection(title: 'Hello World!'), // 時刻とトレーニング開始時刻
           ),
           Expanded(
-            child: MenuList(),
+            child: Log(),
           ), // 筋トレメニューリスト
         ],
       ),
@@ -120,46 +91,11 @@ class _TopSectionState extends State<TopSection> {
   }
 }
 
-// メニューリスト
-class MenuList extends StatelessWidget {
-  final List<String> menu = [
-    'チェストプレス',
-    'ラットプルダウン',
-    'スクワット',
-    'デッドリフト',
-    'ショルダープレス',
-    '腹筋',
-  ];
-
+class Log extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: menu.length, // リストの長さを指定
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: ListTile(
-              tileColor: const Color.fromARGB(255, 255, 255, 255),
-              title: Text(
-                menu[index],
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30),
-              ), // リストの各項目を表示
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                side: BorderSide(
-                  color: Colors.black, // 枠線の色
-                  width: 2, // 枠線の太さ
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LogPage()),
-                );
-              },
-            ),
-          );
-        });
+    return Container(
+      child: Text('Hello!'),
+    );
   }
 }
